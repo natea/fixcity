43e2537476cfd1f96673b6214c05a95e3234092d
Chris Patterson <tenzochris@gmail.com>
2009-12-02 16:06:54 -0500

    [Closes #189] Add images and style hooks for flash messages with different statuses

--- fixcity/flash_messages.py
+++ fixcity/flash_messages.py
@@ -1,7 +1,10 @@
 # flash message wrapper around django flash
 
-def flash(msg, request, msg_type='info'):
+def flash(msg, request, msg_type='confirm'):
     request.flash.add(msg_type, msg)
+
+def flash_info(msg, request):
+    request.flash.add(msg_type, msg, msg_type='info')
 
 def flash_error(msg, request):
     flash(msg, request, msg_type='error')
