e5d2f2fb8ee6926b540873b6ce50ec29c4b05f45
Robert Marianski <rob@marianski.com>
2009-12-02 17:35:27 -0500

    create flash_messages template tag
    create a new template tag "get_and_remove_flash_messages" that retrieves
    all flash messages, and removes them from the store. this prevents the
    issues with messages persisting too long.

---
+++ fixcity/bmabr/templatetags/flash_messages.py
@@ -0,0 +1,32 @@
+# custom flash messages template tag
+
+# can consider moving this out into its own package
+
+from django import template
+
+class FlashMessagesNode(template.Node):
+    def __init__(self, var_name):
+        self.var_name = var_name
+    def render(self, context):
+        # assume that we can access the request
+        request = context['request']
+        flash_messages = dict(request.flash.items())
+        request.flash.clear()
+        context[self.var_name] = flash_messages
+        return ''
+
+def do_flash_messages(parser, token):
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        var_name = 'flash_messages'
+    else:
+        try:
+            as_, var_name = arg.split()
+        except ValueError:
+            raise template.TemplateSyntaxError, "%r tag requires 'as var_name' syntax" % token.contents.split()[0]
+    return FlashMessagesNode(var_name)
+
+register = template.Library()
+register.tag('get_and_remove_flash_messages', do_flash_messages)
