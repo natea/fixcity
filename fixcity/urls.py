c409529233de6767f7f3e26f2e83c1c736378af8
Robert Marianski <rob@marianski.com>
2009-12-03 16:18:08 -0500

    remove code replaced by flatpages
    remove views/urls/templates that are being replaced by flatpages.

--- fixcity/urls.py
+++ fixcity/urls.py
@@ -9,11 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     (r'^$', 'fixcity.bmabr.views.index'),
-    (r'^about/$', 'fixcity.bmabr.views.about'), 
-    (r'^faq/$', 'fixcity.bmabr.views.faq'),
-    (r'^contact/$', 'fixcity.bmabr.views.contact'),
-    (r'^verification-kit/$', 'fixcity.bmabr.views.verification_kit'),
-    (r'^placement-guide/$', 'fixcity.bmabr.views.placement_guide'),
 
     # Account URL overrides.
     # Note these go first because django just iterates over these patterns and uses
