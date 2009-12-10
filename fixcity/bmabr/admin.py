c0f191baaeffb385523f5b922599603b756b16ea
Robert Marianski <rob@marianski.com>
2009-12-10 14:12:44 -0500

    formatting in admin file

--- fixcity/bmabr/admin.py
+++ fixcity/bmabr/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.gis import admin
-from django.contrib import admin as oldAdmin 
 from fixcity.bmabr.models import Rack, Comment, Neighborhoods, CommunityBoard, SubwayStations
+from django.contrib import admin as oldAdmin
 from fixcity.bmabr.models import SeeClickFixSource
 from fixcity.bmabr.models import Source
 from fixcity.bmabr.models import EmailSource
@@ -8,43 +8,35 @@ from fixcity.bmabr.models import TwitterSource
 from fixcity.bmabr.models import  StatementOfSupport
 
 
-#class PhotoAdmin(admin.GeoModelAdmin): 
-#    list_display  = ('rack','email') 
-#admin.site.register(Photo,PhotoAdmin)
+class StatementOfSupportAdmin(admin.GeoModelAdmin):
+    list_display = ('s_rack','email')
 
-
-class StatementOfSupportAdmin(admin.GeoModelAdmin): 
-    list_display = ('s_rack','email') 
-admin.site.register(StatementOfSupport,StatementOfSupportAdmin)
-
-
-class StatementInline(oldAdmin.StackedInline): 
+class StatementInline(oldAdmin.StackedInline):
     model = StatementOfSupport
 
-class SubwayAdmin(admin.GeoModelAdmin): 
+class SubwayAdmin(admin.GeoModelAdmin):
     list_display = ('name','borough')
     search_fields = ('name','borough')
-admin.site.register(SubwayStations,SubwayAdmin)
 
-class CommentAdmin(admin.GeoModelAdmin): 
+class CommentAdmin(admin.GeoModelAdmin):
     list_display = ('rack','email')
-admin.site.register(Comment,CommentAdmin)
 
-class RackAdmin(admin.GeoModelAdmin): 
+class RackAdmin(admin.GeoModelAdmin):
     list_display = ('address','location')
-admin.site.register(Rack, RackAdmin)
 
-
-class NeighborhoodsAdmin(admin.GeoModelAdmin): 
+class NeighborhoodsAdmin(admin.GeoModelAdmin):
     list_display = ('name','county')
-admin.site.register(Neighborhoods,NeighborhoodsAdmin)
 
-
-class CommunityBoardAdmin(admin.GeoModelAdmin): 
+class CommunityBoardAdmin(admin.GeoModelAdmin):
     list_display = ('boro','board','gid')
     ordering = ('boro',)
-admin.site.register(CommunityBoard,CommunityBoardAdmin)
 
+admin.site.register(SubwayStations, SubwayAdmin)
+admin.site.register(StatementOfSupport, StatementOfSupportAdmin)
+admin.site.register(Neighborhoods, NeighborhoodsAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Rack, RackAdmin)
+admin.site.register(CommunityBoard, CommunityBoardAdmin)
 admin.site.register(Source)
 admin.site.register(SeeClickFixSource)
 admin.site.register(EmailSource)
