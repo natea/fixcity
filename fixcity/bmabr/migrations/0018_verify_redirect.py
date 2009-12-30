ff6c031137f4177afc873110305eadfa0e5a6a53
Robert Marianski <rob@marianski.com>
2009-12-30 15:32:48 -0500

    add migration /verify/ -> /racks/

---
+++ fixcity/bmabr/migrations/0018_verify_redirect.py
@@ -0,0 +1,109 @@
+
+from south.db import db
+from django.contrib.redirects.models import Redirect
+from django.contrib.sites.models import Site
+from django.db import models
+from fixcity.bmabr.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        # XXX use the first site
+        site = Site.objects.all()[0]
+        old_path = '/verify/'
+        new_path = '/racks/'
+        if len(Redirect.objects.filter(old_path=old_path)) == 0:
+            redirect = Redirect(old_path=old_path,
+                                new_path=new_path,
+                                site=site,
+                                )
+            redirect.save()
+    
+    
+    def backwards(self, orm):
+        Redirect.objects.filter(old_path='/verify/').delete()
+    
+    
+    models = {
+        'bmabr.borough': {
+            'Meta': {'db_table': "u'gis_boroughs'"},
+            'borocode': ('django.db.models.fields.SmallIntegerField', [], {}),
+            'boroname': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'shape_area': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'shape_leng': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'the_geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {})
+        },
+        'bmabr.cityrack': {
+            'Meta': {'db_table': "u'gis_cityracks'"},
+            'address': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'alt_addres': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'boro_1': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'borocode': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'c_racksid': ('django.db.models.fields.CharField', [], {'max_length': '17'}),
+            'from__cros': ('django.db.models.fields.CharField', [], {'max_length': '22'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '13'}),
+            'large': ('django.db.models.fields.IntegerField', [], {}),
+            'neighborho': ('django.db.models.fields.CharField', [], {'max_length': '21'}),
+            'objectid': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'oppaddress': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'rackid': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'side_of_st': ('django.db.models.fields.CharField', [], {'max_length': '12'}),
+            'small': ('django.db.models.fields.IntegerField', [], {}),
+            'street_nam': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'the_geom': ('django.contrib.gis.db.models.fields.PointField', [], {}),
+            'to__cross': ('django.db.models.fields.CharField', [], {'max_length': '22'}),
+            'x': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'y': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '1000'}),
+            'zip_code_1': ('django.db.models.fields.CharField', [], {'max_length': '12'})
+        },
+        'bmabr.communityboard': {
+            'Meta': {'db_table': "u'gis_community_board'"},
+            'board': ('django.db.models.fields.IntegerField', [], {}),
+            'borocd': ('django.db.models.fields.IntegerField', [], {}),
+            'borough': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bmabr.Borough']"}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'the_geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {})
+        },
+        'bmabr.emailsource': {
+            'address': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'source_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['bmabr.Source']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'bmabr.rack': {
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '300', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.contrib.gis.db.models.fields.PointField', [], {}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bmabr.Source']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
+            'user': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'verified': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'})
+        },
+        'bmabr.seeclickfixsource': {
+            'image_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'issue_id': ('django.db.models.fields.IntegerField', [], {}),
+            'reporter': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'source_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['bmabr.Source']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'bmabr.source': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'bmabr.statementofsupport': {
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            's_rack': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['bmabr.Rack']"})
+        },
+        'bmabr.twittersource': {
+            'source_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['bmabr.Source']", 'unique': 'True', 'primary_key': 'True'}),
+            'status_id': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'user': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+    
+    complete_apps = ['bmabr']
